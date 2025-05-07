354d0f437425ea47ce4633eb28eba5cabba9d25f
Luan Leite <luanleite202@gmail.com>
2025-05-07 10:42:11 -0300

    Login and Signup config

--- Website/auth.py
+++ Website/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 
 auths = Blueprint('auths', __name__)
 
-@auths.route('/login')
+@auths.route('/login', methods=["GET", "POST"])
 def login():
     return render_template("login.html")
 
@@ -10,6 +10,6 @@ def login():
 def logout():
     return render_template("login.html")
 
-@auths.route('/sign-up')
+@auths.route('/sign-up', methods=["GET", "POST"])
 def sign_up():
     return render_template("sign_up.html")
